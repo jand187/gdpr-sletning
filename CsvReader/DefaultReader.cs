91111218e45ecf6ab9e1126ce32ae8f2db792393
Jan Daniel Andersen <jdan@forca.dk>
2019-09-24 10:23:26 +0200

    First commit

---
+++ CsvReader/DefaultReader.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace CsvReader
+{
+	public class DefaultReader
+	{
+		private readonly IReaderOptions options;
+		private readonly IFileHelper fileHelper;
+
+		public DefaultReader(IReaderOptions options, IFileHelper fileHelper)
+		{
+			this.options = options;
+			this.fileHelper = fileHelper;
+		}
+
+		public IEnumerable<string> GetFiles(string input, params IFileFilter<DateTime>[] filters)
+		{
+			IEnumerable<string> rawLines = input.Split(new[] {"\n", "\r\n"}, StringSplitOptions.RemoveEmptyEntries).ToList();
+			var table = new DataTable();
+			
+			if (this.options.FirstRowIsHeader)
+			{
+				var columnNames = rawLines.First().Split(new []{",", ";"}, StringSplitOptions.RemoveEmptyEntries);
+				table.Columns.AddRange(columnNames.Select(c=> new DataColumn(c)).ToArray());
+				rawLines = rawLines.Skip(1);
+			}
+
+			rawLines.Select(l => table.Rows.Add(l.Split(new[] {",", ";"}, StringSplitOptions.None))).ToList();
+
+			var files = table.AsEnumerable().Select(r => r.Field<string>("File Name"));
+
+			return files.Where(f => filters.All(filter => filter.IsValid(this.fileHelper.GetCreateDate(f))));
+		}
+	}
+
+	public interface IReaderOptions
+	{
+		bool FirstRowIsHeader { get; set; }
+	}
+
+	public class FileIsOlderThanFilter : IFileFilter<DateTime>
+	{
+		private readonly DateTime createdDate;
+
+		public FileIsOlderThanFilter(DateTime createdDate)
+		{
+			this.createdDate = createdDate;
+		}
+
+		public bool IsValid(DateTime target)
+		{
+			return this.createdDate > target;
+		}
+	}
+
+	public interface IFileFilter<TEntity>
+	{
+		bool IsValid(TEntity target);
+	}
+
+	public interface IFileHelper
+	{
+		DateTime GetCreateDate(string file);
+	}
+}
---
+++ CsvReaderTests/DefaultReaderTests.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using CsvReader;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace CsvReaderTests
+{
+	public class DefaultReaderTests
+	{
+		private IReaderOptions optionsMock;
+		private IFileHelper fileHelperMock;
+
+		[SetUp]
+		public void SetUp()
+		{
+			this.optionsMock = Substitute.For<IReaderOptions>();
+			this.fileHelperMock = Substitute.For<IFileHelper>();
+		}
+
+		[Test]
+		public void Parse_should_read_headlines()
+		{
+			var target = new DefaultReader(this.optionsMock, this.fileHelperMock);
+
+			//IDataReader reader = target.Parse(new StringBuilder().ToString());
+
+			var dt = new DataTable();
+		}
+
+		[Test]
+		public void GetFiles_should_returns_6_entries()
+		{
+			var target = new DefaultReader(this.optionsMock, this.fileHelperMock);
+			
+			this.optionsMock.FirstRowIsHeader.Returns(true);
+
+			var stringBuilder = new StringBuilder();
+			stringBuilder.AppendLine("Repository;File Name;Status;Comment;");
+			stringBuilder.AppendLine(@"C:\AIPScanner;C:\AIPScanner\testdoc001.docx;Success;");
+			stringBuilder.AppendLine(@"C:\AIPScanner;C:\AIPScanner\testdoc002.docx;Success;");
+			stringBuilder.AppendLine(@"C:\AIPScanner;C:\AIPScanner\testdoc003.docx;Success;");
+			stringBuilder.AppendLine(@"C:\AIPScanner;C:\AIPScanner\testdoc004.docx;Success;");
+			stringBuilder.AppendLine(@"C:\AIPScanner;C:\AIPScanner\testdoc005.docx;Success;");
+			stringBuilder.AppendLine(@"C:\AIPScanner;C:\AIPScanner\testdoc006.docx;Success;");
+
+			var files = target.GetFiles(stringBuilder.ToString());
+
+			Assert.That(files.Count(), Is.EqualTo(6));
+		}
+
+		[Test]
+		public void GetFiles_should_returns_testdoc001_and_testdoc006()
+		{
+			var target = new DefaultReader(this.optionsMock,this.fileHelperMock);
+
+			this.optionsMock.FirstRowIsHeader.Returns(true);
+
+			var stringBuilder = new StringBuilder();
+			stringBuilder.AppendLine("Repository;File Name;Status;Comment;");
+			stringBuilder.AppendLine(@"C:\AIPScanner;C:\AIPScanner\testdoc001.docx;Success;");
+			stringBuilder.AppendLine(@"C:\AIPScanner;C:\AIPScanner\testdoc002.docx;Success;");
+			stringBuilder.AppendLine(@"C:\AIPScanner;C:\AIPScanner\testdoc003.docx;Success;");
+			stringBuilder.AppendLine(@"C:\AIPScanner;C:\AIPScanner\testdoc004.docx;Success;");
+			stringBuilder.AppendLine(@"C:\AIPScanner;C:\AIPScanner\testdoc005.docx;Success;");
+			stringBuilder.AppendLine(@"C:\AIPScanner;C:\AIPScanner\testdoc006.docx;Success;");
+
+			var files = target.GetFiles(stringBuilder.ToString()).ToList();
+
+			Assert.That(files,  Contains.Item(@"C:\AIPScanner\testdoc001.docx"));
+			Assert.That(files, Contains.Item(@"C:\AIPScanner\testdoc006.docx"));
+		}
+
+		[Test]
+		public void GetFiles_should_return_filtered_files()
+		{
+			this.optionsMock.FirstRowIsHeader.Returns(true);
+			var target = new DefaultReader(this.optionsMock, this.fileHelperMock);
+
+			var stringBuilder = new StringBuilder();
+			stringBuilder.AppendLine("Repository;File Name;Status;Comment;");
+			stringBuilder.AppendLine(@"C:\AIPScanner;C:\AIPScanner\testdoc001.docx;Success;");
+			stringBuilder.AppendLine(@"C:\AIPScanner;C:\AIPScanner\testdoc002.docx;Success;");
+			stringBuilder.AppendLine(@"C:\AIPScanner;C:\AIPScanner\testdoc003.docx;Success;");
+
+			this.fileHelperMock.GetCreateDate(@"C:\AIPScanner\testdoc001.docx").Returns(DateTime.Now.FiveYearsAgo().TwoDaysBefore());
+			this.fileHelperMock.GetCreateDate(@"C:\AIPScanner\testdoc002.docx").Returns(DateTime.Now.FiveYearsAgo());
+			this.fileHelperMock.GetCreateDate(@"C:\AIPScanner\testdoc003.docx").Returns(DateTime.Now.FiveYearsAgo().TwoDaysAfter());
+
+			var files = target.GetFiles(stringBuilder.ToString(), new FileIsOlderThanFilter(DateTime.Now.FiveYearsAgo())).ToList();
+
+			Assert.That(files,  Contains.Item(@"C:\AIPScanner\testdoc001.docx"));
+			Assert.That(files.Count(), Is.EqualTo(1));
+		}
+	}
+
+
+	public static class DateTimeExtensions
+	{
+		public static DateTime FiveYearsAgo(this DateTime @this)
+		{
+			return @this.AddYears(-5);
+		}
+		public static DateTime TwoDaysBefore(this DateTime @this)
+		{
+			return @this.AddDays(-2);
+		}
+		public static DateTime TwoDaysAfter(this DateTime @this)
+		{
+			return @this.AddDays(2);
+		}
+	}
+}
