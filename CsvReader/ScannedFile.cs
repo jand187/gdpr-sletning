7259e349427472eb33dc9110fc5435290f799359
Jan Daniel Andersen <jdan@forca.dk>
2019-09-26 11:51:24 +0200

    Added console.

--- CsvReader/ScannedFile.cs
+++ CsvReader/ScannedFile.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Data.SqlClient;
 
 namespace CsvReader
 {
@@ -14,7 +13,7 @@ namespace CsvReader
 		public string CurrentLabelId { get; set; }
 		public string FileName { get; set; }
 		public string InformationTypeName { get; set; }
-		public DateTime LastModified { get; set; }
+		public DateTime? LastModified { get; set; }
 		public string LastModifiedBy { get; set; }
 		public string MatchedInformationTypesString { get; set; }
 		public string MatchedString { get; set; }
@@ -22,32 +21,59 @@ namespace CsvReader
 		public string ProtectionBeforeAction { get; set; }
 		public string Repository { get; set; }
 		public string Status { get; set; }
-		public IFileData FileData { get; set; }
+		public string ParseErrorMessage { get; set; }
+		public bool ParseError { get; set; }
 
 		public static ScannedFile CreateFromCsvLine(string s)
 		{
 			var columns = s.Split(';');
 
-			return new ScannedFile
+			try
 			{
-				Repository = columns[0],
-				FileName= columns[1],
-				Status= columns[2],
-				Comment= columns[3],
-				CurrentLabel= columns[4],
-				CurrentLabelId = columns[5],
-				AppliedLabel= columns[6],
-				AppliedLabelId = columns[7],
-				ConditionName= columns[8],
-				MatchedString= columns[9],
-				InformationTypeName= columns[10],
-				MatchedInformationTypesString= columns[11],
-				Action= columns[12],
-				LastModified = DateTime.Parse(columns[13]),
-				LastModifiedBy= columns[14],
-				ProtectionBeforeAction= columns[15],
-				ProtectionAfterAction= columns[16]
-			};
+				return new ScannedFile
+				{
+					Repository = columns[0],
+					FileName = columns[1],
+					Status = columns[2],
+					Comment = columns[3],
+					CurrentLabel = columns[4],
+					CurrentLabelId = columns[5],
+					AppliedLabel = columns[6],
+					AppliedLabelId = columns[7],
+					ConditionName = columns[8],
+					MatchedString = columns[9],
+					InformationTypeName = columns[10],
+					MatchedInformationTypesString = columns[11],
+					Action = columns[12],
+					LastModified = ParseDate(columns),
+					LastModifiedBy = columns[14],
+					ProtectionBeforeAction = columns[15],
+					ProtectionAfterAction = columns[16]
+				};
+			}
+			catch (IndexOutOfRangeException e)
+			{
+				return new ScannedFile
+				{
+					Repository = columns[0],
+					FileName = columns[1],
+					Status = columns[2],
+					Comment = columns[3],
+					ParseErrorMessage = e.Message,
+					ParseError = true,
+				};
+			}
+		}
+
+
+		private static DateTime? ParseDate(string[] columns)
+		{
+			if (DateTime.TryParse(columns[13], out var date))
+			{
+				return date;
+			}
+
+			return null;
 		}
 	}
 }
---
+++ GdprDeleteFiles/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvReader;
+
+namespace GdprDeleteFiles
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var arguments = ParseArgs(args);
+
+			Console.WriteLine($"Command: {arguments.Command} Filename: {arguments.Filename}");
+
+			var reader = new CsvReader.DefaultReader(null,null);
+			var file = new FileInfo($@"{Environment.CurrentDirectory}\{arguments.Filename}");
+			var createFileSetTask = reader.CreateFileSet(file);
+			createFileSetTask.Wait();
+
+			var fileSet = createFileSetTask.Result.ToList();
+
+			fileSet.ForEach(f => Console.WriteLine(f.FileName + (f.ParseError ? " - *** errors during parsing." : string.Empty)));
+
+			Console.ReadKey();
+		}
+
+		private static CsvReaderArgs ParseArgs(string[] args)
+		{
+			var command = args.First();
+			var options = args.Skip(1);
+
+			switch (command.ToLower())
+			{
+				case "show":
+
+					var index = options.ToList().IndexOf("-f");
+					var filename = options.Skip(index).Skip(1).Take(1).Single();
+
+					return new CsvReaderArgs
+					{
+						Command = command,
+						Filename = filename,
+					};
+
+				case "Delete":
+					return new CsvReaderArgs();
+
+				default:
+					Console.WriteLine("No command was found");
+					return new CsvReaderArgs();
+			}
+		}
+	}
+
+	internal class CsvReaderArgs
+	{
+		public string Command { get; set; }
+		public string Filename { get; set; }
+	}
+
+	internal class DefaultFileHelper : IFileHelper
+	{
+		public DateTime GetCreateDate(FileInfo file)
+		{
+			throw new NotImplementedException();
+		}
+
+		public string ReadAllText(FileInfo file)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
