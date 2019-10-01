b93cd36df35db21fb6b3779893d88b6d62c70e4c
Jan Daniel Andersen <jdan@forca.dk>
2019-10-01 13:32:18 +0200

    GdprService added.

---
+++ GdprService/GdprService.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace GdprService
+{
+	public class GdprService
+	{
+		private readonly IFileHelper fileHelper;
+		private readonly ILogger logger;
+
+		public GdprService(IFileHelper fileHelper, ILogger logger)
+		{
+			this.fileHelper = fileHelper;
+			this.logger = logger;
+		}
+		
+		public void DeleteFiles(IEnumerable<ScannedFile> files)
+		{
+			files.ToList().ForEach(f => this.fileHelper.Delete(f));
+		}
+	}
+
+	public interface IFileHelper
+	{
+		void Delete(ScannedFile file);
+	}
+
+	public interface ILogger
+	{
+		void Log(string message);
+	}
+
+	public class ScannedFile
+	{
+	}
+}
---
+++ GdprServiceTests/GdprServiceTests.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using GdprService;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace GdprServiceTests
+{
+	public class GdprServiceTests
+	{
+		private IFileHelper fileHelper;
+		private GdprService.GdprService gdprService;
+
+		[SetUp]
+		public void Setup()
+		{
+			this.fileHelper = Substitute.For<IFileHelper>();
+			this.gdprService = new GdprService.GdprService(this.fileHelper, Substitute.For<ILogger>());
+		}
+
+		[Test]
+		public void DeleteFile_should_call_FileHelper_Delete()
+		{
+			var scannedFile1 = new ScannedFile();
+
+			this.gdprService.DeleteFiles(
+				new[]
+				{
+					scannedFile1
+				});
+
+			this.fileHelper.Received(1).Delete(scannedFile1);
+		}
+
+		[Test]
+		public void DeleteFile_should_log_error_when_file_not_found()
+		{
+			var scannedFile1 = new ScannedFile();
+
+			this.fileHelper.When(fh => fh.Delete(Arg.Any<ScannedFile>())).Do(fh => throw new FileNotFoundException());
+
+			Assert.Throws<FileNotFoundException>(
+				() => this.gdprService.DeleteFiles(
+					new[]
+					{
+						scannedFile1
+					}));
+		}
+	}
+}
