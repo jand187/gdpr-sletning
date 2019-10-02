474e56fb9052e159f1c54fa54d399ad793c51a61
Jan Daniel Andersen <jdan@forca.dk>
2019-10-02 09:13:31 +0200

    Implemented GdprServiceDeleteFiles.

--- GdprService/GdprService.cs
+++ GdprService/GdprService.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace GdprService
@@ -13,10 +15,26 @@ namespace GdprService
 			this.fileHelper = fileHelper;
 			this.logger = logger;
 		}
-		
+
 		public void DeleteFiles(IEnumerable<ScannedFile> files)
 		{
-			files.ToList().ForEach(f => this.fileHelper.Delete(f));
+			files.ToList()
+				.ForEach(
+					f =>
+					{
+						try
+						{
+							this.fileHelper.Delete(f);
+						}
+						catch (FileNotFoundException e)
+						{
+							this.logger.Log(e.Message, e);
+						}
+						catch (UnauthorizedAccessException e)
+						{
+							this.logger.Log(e.Message, e);
+						}
+					});
 		}
 	}
 
@@ -27,10 +45,11 @@ namespace GdprService
 
 	public interface ILogger
 	{
-		void Log(string message);
+		void Log(string message, Exception exception);
 	}
 
 	public class ScannedFile
 	{
+		public string Filename { get; set; }
 	}
 }
--- GdprServiceTests/GdprServiceTests.cs
+++ GdprServiceTests/GdprServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using GdprService;
 using NSubstitute;
 using NUnit.Framework;
@@ -9,12 +10,14 @@ namespace GdprServiceTests
 	{
 		private IFileHelper fileHelper;
 		private GdprService.GdprService gdprService;
+		private ILogger logger;
 
 		[SetUp]
 		public void Setup()
 		{
 			this.fileHelper = Substitute.For<IFileHelper>();
-			this.gdprService = new GdprService.GdprService(this.fileHelper, Substitute.For<ILogger>());
+			this.logger = Substitute.For<ILogger>();
+			this.gdprService = new GdprService.GdprService(this.fileHelper, this.logger);
 		}
 
 		[Test]
@@ -32,18 +35,60 @@ namespace GdprServiceTests
 		}
 
 		[Test]
-		public void DeleteFile_should_log_error_when_file_not_found()
+		public void DeleteFile_should_catch_and_log_file_not_found_exception()
 		{
-			var scannedFile1 = new ScannedFile();
+			var exception = new FileNotFoundException();
+			this.fileHelper.When(fh => fh.Delete(Arg.Any<ScannedFile>())).Do(fh => throw exception);
 
-			this.fileHelper.When(fh => fh.Delete(Arg.Any<ScannedFile>())).Do(fh => throw new FileNotFoundException());
+			this.gdprService.DeleteFiles(
+				new[]
+				{
+					new ScannedFile()
+				});
 
-			Assert.Throws<FileNotFoundException>(
+			this.logger.Received(1).Log(exception.Message, exception);
+		}
+
+		[Test]
+		public void DeleteFile_should_catch_and_log_unauthorized_access_exception()
+		{
+			var exception = new UnauthorizedAccessException();
+			this.fileHelper.When(fh => fh.Delete(Arg.Any<ScannedFile>())).Do(fh => throw exception);
+
+			this.gdprService.DeleteFiles(
+				new[]
+				{
+					new ScannedFile()
+				});
+
+			this.logger.Received(1).Log(exception.Message, exception);
+		}
+
+		[Test]
+		public void DeleteFile_should_throw_exceptions()
+		{
+			this.fileHelper.When(fh => fh.Delete(Arg.Any<ScannedFile>())).Do(fh => throw new Exception());
+
+			Assert.Throws<Exception>(
+				() => this.gdprService.DeleteFiles(
+					new[]
+					{
+						new ScannedFile()
+					}));
+		}
+	}
+}
+
+
+/*
+ *
+
+	Assert.Throws<FileNotFoundException>(
 				() => this.gdprService.DeleteFiles(
 					new[]
 					{
 						scannedFile1
 					}));
-		}
-	}
-}
+
+ * 
+ */
