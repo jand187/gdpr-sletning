5a69a0ecea73652c2d7ee1b8d5a4db1f99c6ff8a
Jan Daniel Andersen <jdan@forca.dk>
2019-11-25 11:04:13 +0100

    Extracted MoreThanFiveYearsOld.

--- GdprClientConsole/Bootstrapper.cs
+++ GdprClientConsole/Bootstrapper.cs
@@ -64,27 +64,4 @@ namespace GdprClientConsole
 			}
 		}
 	}
-
-	public class MoreThanFiveYearsOld : IFileFilter
-	{
-		private readonly DateTime thresholdDate;
-
-		public MoreThanFiveYearsOld()
-		{
-			this.thresholdDate = new DateTime(DateTime.Today.AddYears(-5).Year, 1, 1);
-		}
-
-		public IEnumerable<ScannedFile> Apply(IEnumerable<ScannedFile> files)
-		{
-			return files.Where(f => f.LastModified < this.thresholdDate);
-		}
-
-		public FilterProcessResult ProcessThisFile(ScannedFile file)
-		{
-			return new FilterProcessResult(
-				new FileInfo(file.Filename).LastWriteTime < this.thresholdDate,
-				$"{file.Filename} is modified after {this.thresholdDate} and should not be deleted.",
-				file);
-		}
-	}
 }
---
+++ GdprClientConsole/MoreThanFiveYearsOld.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using GdprService;
+
+namespace GdprClientConsole
+{
+	public class MoreThanFiveYearsOld : IFileFilter
+	{
+		private readonly DateTime thresholdDate;
+
+		public MoreThanFiveYearsOld()
+		{
+			this.thresholdDate = new DateTime(DateTime.Today.AddYears(-5).Year, 1, 1);
+		}
+
+		public IEnumerable<ScannedFile> Apply(IEnumerable<ScannedFile> files)
+		{
+			return files.Where(f => f.LastModified < this.thresholdDate);
+		}
+
+		public FilterProcessResult ProcessThisFile(ScannedFile file)
+		{
+			return new FilterProcessResult(
+				new FileInfo(file.Filename).LastWriteTime < this.thresholdDate,
+				$"{file.Filename} is modified after {this.thresholdDate} and should not be deleted.",
+				file);
+		}
+	}
+}
